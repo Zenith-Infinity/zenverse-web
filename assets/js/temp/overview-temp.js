96dddfff4457ae225418dfa84bf2f65238ab3111
hrisz <harissaefuloh@gmail.com>
2024-07-04 01:01:22 +0700

    mod: fix button position

--- assets/js/temp/overview-temp.js
+++ assets/js/temp/overview-temp.js
@@ -25,9 +25,10 @@ export let fillTable =
     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
         <div id="gameGenre" class="text-transparent h-2 animate-pulse bg-slate-200 rounded col-span-2">#gameGenre#</div>
     </td>
-    <td
-        class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200 flex justify-center items-center">
-        <a href="editform.html?gameId=#gameEdit#" class="text-white mx-2 px-4 py-1 bg-sky-500 hover:bg-sky-900 rounded-lg">Edit</a>
-        <button type="button" id="deleteButton" onclick="confirmDelete('#gameDelete#')" class="text-white px-3 py-1 bg-rose-600 hover:bg-rose-900 rounded-lg">Delete</button>
+    <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+        <div class="flex justify-center items-center">
+            <a href="editform.html?gameId=#gameEdit#" class="text-white mx-2 px-4 py-1 bg-sky-500 hover:bg-sky-900 rounded-lg">Edit</a>
+            <button type="button" id="deleteButton" onclick="confirmDelete('#gameDelete#')" class="text-white px-3 py-1 bg-rose-600 hover:bg-rose-900 rounded-lg">Delete</button>
+        </div>
     </td>
 </tr>`;
