71d82e96e797d2f0e93f40a8e5efba966577e750
hrisz <harissaefuloh@gmail.com>
2024-10-27 17:50:25 +0700

    add: timeout alert if the page not loaded for too long

--- assets/js/main-controls.js
+++ assets/js/main-controls.js
@@ -1,8 +1,40 @@
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
+import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.9/element.js";
+
+addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+
 let splash = document.querySelector('.splash');
 let logo = document.querySelector('.splash-header');
 let logoSpan = document.querySelectorAll('.splash-logo');
 
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: true,
+    confirmButtonText: "Refresh",
+    timer: 10000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+    }
+});
+
+let loadTimeout = setTimeout(() => {
+    Toast.fire({
+        icon: "warning",
+        title: "Page load is slow. Click 'Refresh' to try again."
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.reload();
+        }
+    });
+}, 5000);
+
 window.onload = () => {
+
+    clearTimeout(loadTimeout);
+
     setTimeout(() => {
         logoSpan.forEach((span, idx) => {
             setTimeout(() => {
