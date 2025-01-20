1f25cd7eaecf41731f2421c226ab0e0eed432747
hrisz <harissaefuloh@gmail.com>
2025-01-20 10:49:10 +0700

    mod: window location for button inside banner

--- assets/js/config/detailed_url.js
+++ assets/js/config/detailed_url.js
@@ -6,10 +6,10 @@ const gameId = localStorage.getItem("selectedGameId");
 
 if (!gameId) {
     alert("No game selected! Redirecting...");
-    window.location.href = "index.html"; // Redirect if no gameId is found
+    window.location.href = "index.html";
 } else if (!/^[a-fA-F0-9]{24}$/.test(gameId)) {
     alert("Invalid game ID! Redirecting...");
-    window.location.href = "index.html"; // Redirect if the gameId is invalid
+    window.location.href = "index.html";
 } else {
     // If valid, construct the API URL
     var urlFetch = "https://zenversegames-ba223a40f69e.herokuapp.com/games/" + gameId;
