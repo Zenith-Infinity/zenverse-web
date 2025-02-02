bd2971ba46549ab5541488b742023fc088e9415b
hrisz <harissaefuloh@gmail.com>
2025-02-03 04:36:49 +0700

    add: encryption id in detail page

--- assets/js/config/detailed_url.js
+++ assets/js/config/detailed_url.js
@@ -14,16 +14,16 @@ if (!gameId) {
     setTimeout(() => {
         window.location.href = "main.html";
     }, 2000);
-} else if (!/^[a-fA-F0-9]{24}$/.test(gameId)) {
-    Swal.fire({
-        icon: "warning",
-        title: "Game unavailable!",
-        text: "The game is not available, ",
-        timer: 2000,
-      });
-      setTimeout(() => {
-          window.location.href = "main.html";
-      }, 2000);
+// } else if (!/^[a-fA-F0-9]{24}$/.test(gameId)) {
+//     Swal.fire({
+//         icon: "warning",
+//         title: "Game unavailable!",
+//         text: "The game is not available, ",
+//         timer: 2000,
+//       });
+//       setTimeout(() => {
+//           window.location.href = "main.html";
+//       }, 2000);
 } else {
     // If valid, construct the API URL
     var urlFetch = "https://zenversegames-ba223a40f69e.herokuapp.com/games/" + gameId;
@@ -32,6 +32,24 @@ if (!gameId) {
         method: "GET",
       })
         .then((response) => {
+          if (response.status === 200) {
+            response.json().then((data) => {
+              var urlEncrypt = "https://zenversegames-ba223a40f69e.herokuapp.com/encrypt?id=" + gameId;
+              fetch(urlEncrypt, {
+                method: "GET",
+              })
+                .then((response) => {
+                  if (response.status === 200) {
+                    response.json().then((data) => {
+                      localStorage.setItem("selectedGameId", data.encrypted_id);
+                    });
+                  }
+                })
+                .catch((error) => {
+                  console.error("Error:", error);
+                });
+            });
+          } else
           if (response.status === 500) {
             Swal.fire({
               icon: "error",
