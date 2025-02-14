a44ef3a31ccd35faa7ba7d2ad37a4c4f0ffeeac2
hrisz <harissaefuloh@gmail.com>
2025-02-14 11:24:37 +0700

    mod: modify else action when the id successfuly detected

--- assets/js/config/put_url.js
+++ assets/js/config/put_url.js
@@ -12,21 +12,7 @@ if (!gameId) {
     alert("Invalid game ID! Redirecting...");
     window.location.href = "dashboard.html";
 } else {
-    // If valid, construct the API URL
     var urlPUT = "https://zenversegames-ba223a40f69e.herokuapp.com/update/" + gameId;
-
-    // Example: Fetch data using the valid urlPUT
-    fetch(urlPUT)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Game Data:", data);
-            // Perform further operations with the fetched data
-        })
-        // .catch((error) => {
-        //     console.error("Error fetching game data:", error);
-        //     alert("Failed to fetch game details. Redirecting...");
-        //     window.location.href = "dashboard.html"; // Redirect on fetch failure
-        // });
 }
 
 export { urlPUT };
